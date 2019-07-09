e5bcd20f9b9741912d5ff96c409c80f25472548d
mattig94 <mattig94@users.noreply.github.com>
2019-07-09 13:14:32 -0400

    adding JavaScript validation

---
+++ js/script.js
@@ -0,0 +1,65 @@
+(function() {
+	var $form = document.querySelector('#contact-form');
+	var $emailInput = document.querySelector('#email');
+	var $messageInput = document.querySelector('#message');
+
+	function showErrorMessage($input, message) {
+		var $container = $input.parentElement;
+		
+		//Remove an existing error
+		var error = $container.querySelector('.error-message');
+		if (error) {
+			$container.removeChild(error);
+		}
+
+		//Add error message if error
+		if (message) {
+			var error = document.createElement('div');
+			error.classList.add('error-message');
+			error.innerText = message;
+			$container.appendChild(error);
+		}
+	}
+
+	function validateEmail() {
+		var value = $emailInput.value;
+		
+		if (!value) {
+			showErrorMessage($emailInput, 'Email is a required field');
+			return false;
+		}
+		if (value.indexOf('@') === -1) {
+			showErrorMessage($emailInput, 'Please enter a valid email address');
+			return false;
+		}
+		showErrorMessage($emailInput, null);
+		return true;
+	}
+
+	function validateMessage() {
+		if ($messageInput.value === -1) {
+			showErrorMessage($messageInput, 'Please include a message');
+			return false;
+		};
+		showErrorMessage($messageInput, null);
+		return true;
+	}
+
+	function validateForm() {
+		var isValidEmail = validateEmail();
+		var hasMessage = validateMessage();
+		return isValidEmail && hasMessage;
+	}
+
+	$emailInput.addEventListener('input', validateEmail);
+	$messageInput.addEventListener('input', validateMessage);
+
+	$form.addEventListener('submit', (e) => {
+		e.preventDefault();
+		if (validateForm()) {
+			alert('Success!');
+		}
+	})
+
+}) ();
+
